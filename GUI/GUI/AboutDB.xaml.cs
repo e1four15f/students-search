3a7a507519d2ca9d5d49fa6dd604a60c33966309
e1four15f <Tesskyrim000@gmail.com>
2018-10-28 14:14:33 +0300

    Refactored project

--- GUI/GUI/AboutDB.xaml.cs
+++ GUI/GUI/AboutDB.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication1
+namespace GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AboutDB.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AboutDB : Window
     {
-        public MainWindow()
+        public AboutDB()
         {
             InitializeComponent();
         }
--- GUI/GUI/Settings.xaml.cs
+++ GUI/GUI/Settings.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication1
+namespace GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Settings.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Settings : Window
     {
-        public MainWindow()
+        public Settings()
         {
             InitializeComponent();
-
-           
         }
     }
 }
